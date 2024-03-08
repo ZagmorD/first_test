6d4e635985afe8cef72a14121428af1b8766680e
ZagmorD <161042364+ZagmorD@users.noreply.github.com>
2024-03-08 21:59:37 +0300

    Create lesson_1_5.py

---
+++ lesson_1_5.py
@@ -0,0 +1,61 @@
+# Задание 5.1
+class torp_air:
+    
+    def __init__(self, torp_air_numb_plan,torp_air_atack_gr, torp_air_torp,torp_air_speed,torp_air_det_dis):
+        self.__num_plan = torp_air_numb_plan # общее количество самолётов
+        self.__atack_gr = torp_air_atack_gr # количество атакующих групп
+        self.__torp = torp_air_torp # количество торпед на одном самолёте
+        self.__speed = torp_air_speed # скорость самолёта
+        self.__det_dis = torp_air_det_dis # дальность обнаружения самолётов
+    
+    def __Attack__(self): #в питон тьюторе для создания праватного метода 2 подчёркивания нужны не только в начале, но и в конце
+        self.__atack_gr -= 1 # от общего количества отделяется 1 ударная группа
+        self.__torp = 0 # сбрасываются торпеды с отделившихся самолётов
+        
+B7A = torp_air(8, 4, 2, 137, 7)
+B7A.__Attack__()
+
+# Задание 5.2
+# Первая пара классов с наследованием
+class aviation:
+    def __init__(self,air_numb_plan, air_atack_gr, air_speed, air_weapon, air_det_dis):
+        self.num_plan = air_numb_plan # общее количество самолётов
+        self.atack_gr = air_atack_gr # количество атакующих групп
+        self.speed = air_speed # скорость самолёта
+        self.weapon = air_weapon # количество вооружения на одном самолёте
+        self.det_dis = air_det_dis # дальность обнаружения самолётов
+
+    def Attack(self):
+        self.atack_gr -= 1 # от общего количества отделяется 1 ударная группа
+        self.weapon = 0 # сбрасываются торпеды с отделившихся самолётов
+
+class torp_air(aviation):
+
+    def __init__(self, torpedos, air_numb_plan,air_atack_gr, air_speed, air_weapon, air_det_dis):
+        super().__init__(air_numb_plan,air_atack_gr, air_speed, air_weapon, air_det_dis)
+        self.type_weapon = torpedos
+        
+B7A = torp_air('Торпеды', 4, 2, 137, 2, 7)
+B7A.Attack()
+
+# Вторая пара классов с наследованием
+class animal_transport_container: # просто переноска
+    def __init__(self, l, t, h, g, name_animal):
+        self.dlina = l 
+        self.shirina = t 
+        self.visota = h
+        self.napolnennost = g # 0 - переноска пуста, 1 - в переноске животное
+        self.name = name_animal # Имя того, кто сидит в переноске
+        
+    def zahvat(self, name_animal):
+        if self.napolnennost == 0:
+            self.napolnennost = 1
+            self.name = name_animal
+
+class dragon_container(animal_transport_container): # переноска для дракона
+    def __init__(self, ne_gorit, l, t, h, g, name_animal):
+        super().__init__(l, t, h, g, name_animal)
+        self.ne_gorit = ne_gorit
+
+kletka = dragon_container('Yes', 100, 100, 100, 0, '')
+kletka.zahvat('Drakon') # захватываем дракона Дракон :)
